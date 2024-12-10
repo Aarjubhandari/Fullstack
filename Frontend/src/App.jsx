0d526488821ec343b33e8a1d6536d6b7f16abe72
aarju bhandari <aarjubhandari000@gmail.com>
2024-12-10 14:00:36 +0545

    full stack

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import axios from 'axios';
+
+function App() {
+  const [jokes, setJokes] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/jokes')
+      .then((response) => {
+        setJokes(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  })
+
+  return (
+    <>
+      <h1>Fullstack JavaScript</h1>
+      <p>JOKES: {jokes.length}</p>
+
+      {
+        jokes.map((joke, index) => {
+          return (
+            <div key={joke.id}>
+              <h3>{joke.title}</h3>
+          
+            </div>
+          );
+        })
+      }
+    </>
+  );
+}
+
+export default App;
